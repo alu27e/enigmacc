c99007800e0695c2f6af92c9028cc5ff49a2bd7e
alu <al.u27e@gmail.com>
2026-06-18 12:49:06 +0200

    enigmacc.c

---
+++ enigmacc.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifdef _WIN32
+#define popen _popen
+#define pclose _pclose
+#endif
+#define O(...)
+#define V(...)
+#define W(...)
+#define L(...)
+#define T(x)#x
+#define S(x)T(x)
+                                                                       enum{MW=62,MH=31}; L(i+j,p^s,n?x:z,~q,r[7) char*Q[]
+                                                                ={S(R&;A%?A~(x)A!;-%if(x)-R;+|?Z!elseR<;*|?QA;Q-?QZ(x)A*;A>)S(*A;
+                                                           R&?%Q),S(QQ;A~?QZ(x)Q=;%%if(x)RA;R=?R*else+-;+|?+%;-Z?-A(x)ZZ;R<)S(R&;R~?Z!
+                                                      (x)+&;QZif(x)Q*;A!?Z=    else%|;*-?*=;*+?+|(x)+|;+Q)S(A-),S( A=;Q&?%%(x)%^;*Zif(x)ZZ;
+                                                  RQ?Q|else+&;-^?-Z;Z-?Z+(x)RA         ;ZR)S(R%;Q* ?A>(x)          -=;A-if(x)%R;*>?ZQelse**;*>?
+                                               +~;+=?-~(x)--;Z-)S(*&;*~  ?Z*),         S(%Z;%~?%>(x)%A;%%       if(x)+|;+  |?A<else-+;ZQ?Z-;Z^),S(
+                                           RR;*Z?*>),S(ZQ;Q!?-<(x        )+R;Q*        ),S(+=;--?-*(x)-^;*      &if(x)ZR;       R-?R!elseQ=;QA?QA;AR),
+                                        S(-Z;-!?RQ(x)Z^;%>if            (x)Z!;Q<?      A%elseQZ;A+?Q-;*Z?*     &(x)%=;%=)     ,S(Z~;R&?R+(x)R<;*Zif(x)++;
+                                     +Z?Q+elseQ<;%&?%R;% !?%Z(          x)*=;Q^),S(    R+;R*?Z ^(x  )Q+;+      Aif(x)A=;A   ~?*!else*=;  RZ?R+),S(*A;Q!?-|(x
+                                   )+!),S(-*;-<?AQ(x     )+=;R&        ),S(A+;AA?*&     (x)%!;%>if(x)%>;       --?+=else+   ~;A*?+=;RA?      RA(x)Z&;R-)S(Q-;A
+                                +),S(*-;*&?**(x)%Q;-*    if(x)+Z;Q<?+  -else  +|;R|?RQ;R=?R=(x)Z!),S(*+;*~?%^(x)-+;+Rif(x   )--;-* ?Z%      else %R;%Q),S(-|;+^?+
+                              R(x)-%;A^if(x)Z=   ;R=?Z!   elseR=;%=?Q  -;Q-?A*(x)-|;*&)S(**;%<?*-(x)*-;QRif(x)-|;-<?-+      else+|;Z>?     ZR;RR    ?%^),S(Q<;-*),S
+                            (A=;+!),S(*Q;R> ?%*(x)Z|;++    if(x)Q~;+^?A-),S(R!;R*?+Q(x)A+;A~if(x)A*;A+?ZRelse%>;*Z?%=;*=),S(%^;Q-?R*(x   )+~;+|     if(x)R%;*-),S(+|;
+                          AQ?ZA(x)R<;R^     if(x)R ^;%<)    ,S(-!;*R?-!(x)R=;Z=if(x)Z%;AA?AAelseA+;-!?Q+;*R?Z|(x)*>;%<)S(Q*;Q+?--(x)A+   ),S(ZR    ;*<?+>(x)Z~;+>),S(R*
+                        ;%|?%^(x)Q=;+~      if(x)%%),S(Q   -;++?%-(x)AQ;-&if(x)Q%),S(Q&;-               %?*Q(x)*%;Z=if(x)AZ),S(A>;ZZ?    AR(x)   *^;RZif(x)A<)},*X[999],*
+                      Y[999],*Z[999],*D=     S(~[;7 Q?%<(x)3 X;R^if(x).6)S(D*(                                     x)=2;9 Zif(x)|:;5 E?*!)S(]  8 if(x)A&;>4?  P+else~[;7 Q)
+                     S(%<?3 XelseR^;.6?D*;=2  )S(9 Z;|:?5 E;-!?]8(x)A&)S                                                 (>4;P+?~[(x)7 Q;%<if  (x)3 X)S(R^(x    ).6;D*if(x)=2
+                   ;9 Z?|:)S(X^  if(x)-!;]8?A     &else>4;P+)S(~[?7 Qelse%                                             <;3 X?>~;.6)S(D*;=2?9 Z;|:?5 E(x) -!)   S(]8;A&?>4(x)P+
+                  ;~[if(x)7 Q    )S(%<(x)3 X;  R^if(x).6;D*?=2)S(9 Zif(x)>2;RD                                     ?-=else29;Z|)S (:4?P+else~[;7 Q?%>;5      E)S(|~ ;.6?D*;=2?9
+                Z(x)|:)S(5 E     ;-!?]8(x)A&;>4 if(x)P+)S(~[    (x)7 Q;%<if(x)3 X;                             R^?.6)S(Q[if(x)]7;    Q*?=[else7Q;%<)S(3   X?R^else]   9;R:?![;7 A
+               )S(|:;6 Q?>       ^;.6?D*(x)=2)S(9 Z;|:?5       E(x)-!;]8 if(x)A&)S(>4(x                    )P+;~[if(x)7 Q;%<?4 Z)S      (&!if(x)]9;Z|?    >4 elseP+;~[)S(7 Q?%< L()
+             else3X;R^?[          7;D&)S(.E;PZ?|:;6&?R^        (x).6)S(D*;=2?9 Z(x)|:;5 Eif           (x)-!)S(]8(x)A&;>4 if(x)P+;~         ]?D+)S(|.if(x  )5 E;-!?]8     elseA&;>4)S
+            (P+?~[ O(]) else7Q;%<?3    X;R^)S(.6;Q*?           :5;EZ?|:(x)4 X)S(R^;.6?D*(x)=2;9   Zif(x)|:)S(5 E(x)-!;]8 if(x)A&;>5          ?R*)S(<2 if(x)9 Z;|:?5 O()   Eelse-!;]8)
+           S(A&?>4      elseP+;~[?7  Q;%<)S(3 X;R^            ?.6;D*?:6(x)ER)S(=<;3 X?R^(x).6;D*if(x)=2)S(9 Z(x)|:;5 Eif(x)-!;]8?%^            )S([6 if(x)D*  ;=2?9 O()     Zelse|:;5
+          E)S(-!?]8     elseA&;>4?P+;~[)S(7 Q;%<              ?3 X;R^?.6(x)D*)S(.6;8 Q?%<(x)3 X;R^if(x).6)S(D*(x)=2;9 Zif(x)|:;5 E?*              .)S(4 Xif(x )R^;.6    ?D*  else=2;9 Z
+         )S(|:?5         Eelse-!;]X?A|;3 X)S(R               -;![?7 Q;%<?3 X(x)R^)S(.6;Q|?![(x)7 Q;%<if(x)3 X)S(R^(x).6;D*if(x)=2;9                 Z?|.)S(9 Aif(x      )%<;3 X?R^else.6
+        ;D*)S(=2?9        Zelse  |:;5 E?%[;7                 Q)S(&>;4 P?+^;.2?7  Q(x)%<)S(3 X;R^?.6(x)D-;~[if(x) 7 Q)S(%<(x)3 X;R^if(                x).6;D*?=2)     S(9%if(x)! [;7 Q?%<
+       else3X;R^) S(.6?D   *    else=2;9 Z                  ?|4;X+)S(!2;9 Z?&<;3 E?R^(x)=[)S(7 Q;%<?3 X(x)R^;.6 if(x)E+)S(~[(x)7 Q;%<                  if(x)3 X;R   ^?.6)S(D*if  (x)75;P+?
+      ~[else7Q; %<)S(3 X?R     ^else.6;D*                   ?=2;P%)S(>6 ;D*?+~;9 A?&:(x)59)S(R+;~[?7 Q(x)%<;3 Xif(x)R^)S( .5(x)P+;~[if                  (x)7 Q;%<?3 X)S(R^if(x)   .7;R&?>4
+     elseP+;~[) S(7 Q?%<      else3X;R^                    ?.6;D*)S(>8;Q|?&6;D*?=~(x)8 A)S(&=;.4?P+(x)~[;7 Qif(x)%<)S(3 X(x)R!;>4 if(x                    )P+;~[?7 Q)S(%<   if(    x)3 X;R~?
+     ]8 elseA& ;>4)S(P+?~[   else7Q;%<                     ?3 X;R^)S(.6;D&?[7;Z^?.>(x)4*)S(=];      6 Q?A&(x)>4;P+if(x)~[)S(7 Q(x)%<;3                     Pif(x)%&;>       4?P+)S  (~[if(x)7
+    Q;%<?3&    else-!;]8)S( A&?>4 O()                     elseP+;~[?7 Q;%<)S(3 X;R^?.6;A%?              [8(x)A&)S(>4;PD?-!(x)]8;A&if(x)                     >4)S(P+(x)  ~[;7 Qif(x   )%<;3 Q?
+   A&)S(>4     if(x)P+;~[?7 Qelse%>;                    5 E)S(-!?]8 elseA&;>4?P+;~[)S                       (7 Q;%<?3 X;R^?.6(x)D*)S(=3;                      XZ?|:(x)5 E;-!if(x)]   8)S(A&(x)
+   >4;P+if(     x)~[;7 Q? %>)S(78                   if(x)A&;>4?P+else~[;7 Q)S(!:?                               8 Eelse-!;]8?A&;>4)S(P+;~[?7 Q                 ;%<?3 X( x)R^)S(.      6;D*?=2(x
+  )9 Z;|:    if(         x)5 E)S(-              !(x)]8;A&if(x)>4;P+?~[)S(7                                          Qif(x)&6;]8?A&else>4;P+)S(~[?8              -else~]; 8 Q           ?-<;3 X)
+  S(R&;>4? P+;~[  ?7 Q   (x)%<)S(          3 X;R^?.6(x)D*;=2 if(x)9 Z)S(|      :(x)5 E;-!                if(x)]8;A       &?>4)S(P+if(x)~[;7 Q?*!else]8          ;A&)S(>4?  P+else~[;D+ ?~];8 Q)S
+ (%<;]8?A  &;>4?  P+(x  )~[)S(7        Q;%<?3 X(x)R^;.6 if(x)D*)S(=2(          x)9 Z;|:if                (x)5 E;-!           ?]8)S(A&if(x)>4;P+?~[else9E;-       !)S(]8?A  &else>4;P+?  ~[;--)S(
+ =[;5 E?-  !;]8? A&(x) >4)S(P+;    ~2?9 Z(x)%<;3 Xif(x)R^)S(.6(x)D*            ;=2 if(x)9                Z;|:?5 E)             S(-!if(x)]8;A&?>4 elseP+;!4)S(5    E?-!else ]8;A&?>4;P+   )S(~P;A~
+?=];8 A?   &>(x)5 P)S( +!;[7?Q*(x)=[;7 Qif(x)%<)S(3 X(x)R^;.6 if(x               )D*;=2?                  9 Z)S(|              :if(x)5 Q;&<?]8 elseA&;>4)S(P+?^:else5E;-!?   ]8;A&)S(>   4;P+?4 P
+;+~?>7(x   )P%)S(|:;PE?+~(x).4;P+if(x)~[)S(7 Q(x)%<;3 Xif(x)R^;.6?               D*)S(=                   2 if(x               )9 Z;|:?7 Aelse&<;]8)S(A&?>5 elseAZ;|:?5 E;-  !)S(]8;      A&?>4;P
++?~[(x)     8*)S(~2;9 Z?%<(x)3 X;A&if(x)>4)S(P+(x)~[;7 Qif(x)%<;3                X?R^          )S(        .6 if                 (x)D*;=2?9  Zelse|[;8 Z)S(&!?]8 elseA&;>3?9  Z;|:)        S(5 E;-
+!?]8;A&               ?>4(x)|^)S(:4;D!?![(x)7 A;%!if(x)]8)S(A&(x)                              >4;                              P+if(x)~[;7 Q?%<)S(3 Xif(x)R^;.6?D*else=2;9  Z)S          (~[?9
+Zelse-!              ;]8?Z[;= 2)S(9 Z;|:?5 E;-!?]8(x)A<)S(]8;A|?                               .4(                               x)P+;~[if(x)5 E)S(-!(x)]8;A&if(x)>  4;P+?~[               )S(7
+Qif(x)               |<;3 X?    R^else.6;D*)S(=2?P-else~2;9 D?-!                         ;2  E)S(D[;=2?9                         Z;|:?5 E(x)-!)S(]Q;-!?]Z(x)Z&;>4    if(x)PZ               )S(|:(
+x)5 E;               -!if(x)     ]8;A&?>4)S(P+if(x)~[;9 R?%< O()  else3X;R^)S(.6?D*      else>4;P*?=.;5       E)S(->;8 D?D*;=2?9  Z(x)|:)S(5 E;-!?2 Q(x)=<;]8 if     (x)A&)S               (>2(x)
+9 Z;|:               if(x)5        E;-!?]8)S(A&if(x)>4;P+?~3      elseXQ;%<)S(3 X?R^     else]9;R^?.5;9       Z)S(|:;5 Q?|=;.6?D* (x)=2)S(9 Z  ;|:?5 E(x)-!;2        Eif(x)-               !)S(]8
+(x)A*;  =2           if(x)9          Z;|:?5 E)S(-!if(x)]8;A&?>4   elseP+;=3)S(XR?^.else6Q;%<?28;D%)S(=2;9 Z?|[;9 D?R^(x).6)S(D*;=  2?9 Z(x)|:;5 Eif(x)~:)S(6          E(x)-!  ;]6 if(x)D*  ;=2?9
+Z)S(|:  if(x)5 E;-!  ?]8 O()          elseA&;>4)S(P+?=2 elseXR    ;|:?3 X;R^)S(.6;D*?=2;9+?=.(x)3 X)S(R^;.6?D*(x)=2;9 Zif(x)|:)S(  RZ(x)^:;5 Eif(x)-!;.6?D*           )S(=2   if(x)9 Z;|:  ?5 O()
+Eelse-  !;]8)S(A&?>  4 elseP            +;~[?7 Q;%<)S(3 X;R^?.      6;D*?=  5(x)D%)S(%<;3 X?R^(x).6;D*if(x)=2)  S(9 Z(x  )^2;9      Zif(x)|:;5 E?-^)S(.6              if(x)D  *;=2?9 V(,)  Zelse|
+:;5 E)  S(-!?]8 V()  elseA&;             >4?P+;~[)S(7 Q;%<?3        X;R^?   .6(x)        A^)S(>3),*G=S(         94^&<    [}38       d]_{%*-=~e!-=]~49%&*             <[{_^38  cbae*&%-=]~  4 abc<
+8 e!-=  ]~49%        &*<[}38               cbae!-=]~49%&*+^_{       W(8)    L(<)         abcde!-=]~49%&*        +^_{     e*&%        -=]~4 abcd8)S(3}{               _<e!-=]  ~49^_{}+<[   38 de%
+&*:?     L()         _16bace                 &*+ba4;|{~27 e!                             -=]~49?;+17 d^e                             !-=]~49 abcde!-=                ]~49._;  +<[}   38 d  ?e!-=]
+~49.d83         }[<+ ?{e&*<[}                 38)S(cba4~]=-%e!                                 -=]                                 ~49%&*<[{_^e&*<[                  }3 dba4               ~]=-%
+17 e!-=   ]~49%&*<[{  _^7 d1e                   +*&%-=^_{38 O(9)                               O()                                cba9e!%&*+:?                      _16be!-= ]~4    V(,)  abc83}[
+<+e!-b<   +=[]}~357)S (e!-=]                    a1_c}[<+~3468 e!>                              _2d                              +|09-<48 e!>_+|16                   b-<e!%& *+|_09abcd<4  e&*<[}
+38 cba4~  ]=-%|_0e&:? _16bac.e                 %&*<[{159 abcd-e!%&*                                                           <[38{_^cba)S(9 e3{~                  012*:;>| ]7 ce+*&%!-=  ]^_{38
+cba9e*&%  -=]~4^_{38   cbae!%&*                +|_05<ae&*<[{_^=-~4                          abc83%                           e_^=-%&*<[}38{abce*&%                 b-<[{1 e ),*A=S(QAZR  )S(+-*%)
+ S(&|^~)S (!=<>)S(:.[  ])S(2345               )S(6789)S(XPED),*B=S(!%&                      *+-.:;<=>                      ?|[]^_{}~0123456789 W(a)               abcde);          int   P[]={45,
+ 38,55,    255,         148,206,              255,168,222,230,125,185,                      198,104,                      154,155,82,124,248,248,                252,205,          205, 218,118,
+  118,150,              36,36,80,            186,169,105, 103,76,47},x,z;                   char*F(                     int n){static    char b[4][              256];                  static
+  int k;                 char*p=Q[           n],*q=b[k++&3],*r=q,*a=A,*h,*l                   ;int i                  ;for(i=0;;i++){while(*p&&!(h=             strchr(a               ,*p)))p
+   ++;if(!*               p)break;          p++;while(*p&&!(l=strchr(a,*p)))p                 ++;if                  (!*p)break;p++;*q++=(((h-a)<<4)|          (l-a))^(               (n*13+i*7
+   +91)&127)          ;}*q =0; L(-)         return r;}void H(int m){clock_t e=                O(b)                 clock()+m*CLOCKS_PER_SEC/1000; O()         while(clock()           <e);}int
+    U(int c){   O() return c>96&&c<         123?c-32:c;}int I(int c){c=U(c); O()                                  return c>64&&c< 91?c-65:c>47&&c<58?c       -22:36;}int Cc(char     *g,char*
+     s){int i   ,j=0;for(i=0;s[i]&&j<      120;i++)if(s[i]>32&&s[i]<127)g[j++]=s[i];if(!j)g[j++]=42;g[j]=0;return j;}char Nn(char*g,int l,int n){char      c=g[n%l];return n&1?U(   c):c>64&&
+     c<91?c+32  :c;}char Ee(char*s,int     n){for(;*s;s++)if(strchr(A,*s)&&!n--)return*s;return*A;}int Vv(char**m){char*q;while(**m){q=strchr(B,*(*m)++   );if(q)    return q-B;}  return 35
+      ;}void Bn (char*s){char g[128],*m;  int l=Cc(g,s),i,r,j,v,id,x,y;putchar(10);for(r=0;r<8;r++)putchar(10);printf(F(24),27,8);for(i=0;g[i]&&i<18;i   ++){id=I(g  [i]);for(m=G ;id;)if(
+       Vv(&m)==  35)id--;x=y=   0;while((v=Vv(&m))<35){r=v/5;j=v%5;if(r>y)printf(F(26),27,r-y);else if(y>r)printf(F(24),27,y-r);if(i*6+j>x)printf(F(27),27,i*6+j-x   );else if(x >i*6+j)
+        printf(F( 25),27,        x-i*6-j);printf(F(22),27,255,148,206,Nn(g,l,i+r+j));x=i*6+j+1;y=r;fflush(stdout);H(35);}if(y)printf(F(24),27,y);if(x)printf(F(  25),     27,x) ;H(180);}
+         printf(F(        26),27,8);putchar(10);}void Oo(int c){putchar(c);fflush(stdout);H(18);}void Bb(){char s[64];int i,n;strcpy(s,F(15));n=strlen(s); V(,)  L(e|)         printf(F(
+          26),27,2);   printf(F(27) ,27,MW-1);                              Oo(46);for(i=0;  i<n+4;i  ++)Oo(45);Oo(46)                            ;printf(F(25),27,n+6)      ;printf(F(
+           26),27,1);  Oo(111);Oo(   40);Oo(32);                             for(i=0;s[i];i++)Oo(s[i]);Oo(32);Oo(41)                            ;printf(F(25 ),27,n+5);     printf(F(
+            26),27,1);Oo(111);Oo(      96);for(i=0;i                           <n+3;i++)Oo(45);Oo(39);printf(F(25),                           27,n+6); L()   printf(F(24   ),27,4);
+             printf(F(25   ),27,     MW-1);}void K(int                          f){char g[128],c;int l=Cc(g,Y[f])                          ,i,j,v,*p,r,n,o,  Rr[MW*MH];  printf(F(20
+               ),27);Bn(Y[ f]);      for(i=n=0;i<MH;i++)                          for(j=0;j<MW;j++){c=Ee(D,i*MW                          +j);v=(strchr( A,c)-           A-(((i*MW+j
+                )*7+11)&31) )&       31;if(v)Rr[n++]=i*MW+j;                        }for(i=0;i<MH;i++)putchar(                       10);printf(F(24)   ,27,MH)       ;for(i=n-1;
+                 i>0;i--){j=          rand()%  (i+1);r=Rr[i];Rr                      [i]=Rr[j];Rr[j]=r;}for(                      o=0;o<n;o++){r=     Rr[o];i=r/    MW;j=r%MW;c=
+                   Ee(D,r);v=(      strchr(A,    c)-A-((r*7+11)&31)                    )&31;p=P+(v-1)*3;if                    (i)printf(F(26),27      ,i);if(j)   printf(F(27)
+                    ,27,j);printf   (F(22),   27,p[0],p[1],p[2],Nn(g,l,r                ));printf(F(25),                  27,j+1);if(i)printf(F(24    ),27,i);fflush(stdout);
+                      H(3);}printf( F(23),  27);Bb();printf(F(26),27,MH); L()             putchar(10);              printf(F(18),F(1),X[f]   ,10,10   );printf(F(21),27);}
+                        char*J(char*u){     char q[999],  b[4096],*o=0;size_t n=0,c=0       ,g;FILE*p;     sprintf(q,F(0),u);p=popen(q,"r"   );if(!p)  return 0;while((g=
+                          fread(b,1,4096    ,p))){if(n+g    +1>c){c=c?c*2:8192;while(  c<n+g+1)c*=2;o= L() realloc(o,c);}memcpy(o+n,b,g);n    +=g;}       pclose(p);if(!
+                            o)o=calloc(1,1  );o[n]=0;      return o;}char*R(char*s,char*k){char*q=strstr(s,k),*e;if(q){q+= O(t)  strlen(k);     if((e  =strchr(q,'"'))
+                              )*e=0;}return q;}int C(      char*s){     int i;for(i=0;i<z;i++)if(!strcmp(s,Z[i]))return 1 ;      return 0;}      void M(char*h){
+                                char*t=h,*e,*q;while((   t=strstr(t,F          (4)))){if(!( e=strstr(t,F(5)))) O() break; *e=0;  if(!strstr(t    ,F(6))&&!strstr(
+                                  t,F(7))&&(q= V(,)      strstr(t,F(8          )))){  q+=15   ;*         W(w)      strchr(q,'"'     )=0;X[x]= q;Y[x]=R(t,F(9));
+                                     if(!Y[x])Y[x]=q;x   ++;}t=e+5;            }}void N(      char       *h){      char*p=h,*e,*    n,*q,*r,*w=h+strlen(h);
+                                        while((p=strstr(p,F(10)))){             e=strstr(p,   F(11       ));       if(!e)break;*    e=0;n=strstr(e+5,F(10)
+                                           );if(!n)n=w;if(strstr                (p,F(12))     ||       strstr(p,F   (13)))for(p= e+5;(q=strstr(p,F(8))
+                                              )&&q<n;p=r+1){q+=15;r=             strchr(      q,      '"');*r=0;Z    [z++   ]=q;}p=n;}}int main(int
+                                                  c,char**v){char u[512],s[       777],       *a,*    b;int i,j;    if(c>1)sprintf(u,F(17),v[1])
+                                                     ;else{printf(F(14));fgets(u,512,stdin)    ;      for(i=0;u[i]&&u[i]!=10&&u[i]!=13;i++);
+                                                          u[i]=0;}a=J(F(1));M(a);sprintf(s, F(2),u);b =J(s);if(!strstr(b,F(3)))return
+                                                               puts(F(19)),1;N(b);for(i=j=0;i<x;i++)if(!C(X[i]))X[j]=X[i],Y[j++]=Y
+                                                                      [i];x=j;srand(time(0));if(x)K(rand()%x);return 0;} W()
